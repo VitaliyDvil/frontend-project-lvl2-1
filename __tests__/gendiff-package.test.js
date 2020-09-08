521f617cdfa2eaff15162c81d6bdc1281fc6e7f8
looleeluu <ilya.sinegubkin@gmail.com>
2020-09-08 23:08:29 +0300

    edit test and readme

--- __tests__/gendiff-package.test.js
+++ __tests__/gendiff-package.test.js
@@ -4,14 +4,11 @@ import genDiff from '../src/gendiff-package.js';
 
 const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
 
-test('gendiff with json files', () => {
+test('gendiff testing', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  const filepath3 = getFixturePath('file3.yaml');
+  const filepath4 = getFixturePath('file4.yaml');
   expect(genDiff(filepath1, filepath2)).toBe(result);
-});
-
-test('gendiff with yaml files', () => {
-  const filepath1 = getFixturePath('file1.yaml');
-  const filepath2 = getFixturePath('file2.yaml');
-  expect(genDiff(filepath1, filepath2)).toBe(result);
+  expect(genDiff(filepath3, filepath4)).toBe(result);
 });
