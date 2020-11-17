e6215a7962e6a41f9586bbb8f1855b1eba5ca8dc
looleeluu <ilya.sinegubkin@gmail.com>
2020-11-18 00:00:30 +0300

    refactoring unit-test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
+/* eslint-disable max-len */
 /* eslint-disable no-underscore-dangle */
 import path, { dirname } from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,17 +11,22 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getResult = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each([
-  ['file1.json', 'file2.json', 'stylish', 'stylish-result.txt'],
-  ['file1.yaml', 'file2.yaml', 'stylish', 'stylish-result.txt'],
-  ['file1.ini', 'file2.ini', 'stylish', 'stylish-result.txt'],
-  ['file1.json', 'file2.json', 'plain', 'plain-result.txt'],
-  ['file1.yaml', 'file2.yaml', 'plain', 'plain-result.txt'],
-  ['file1.ini', 'file2.ini', 'plain', 'plain-result.txt'],
-  ['file1.json', 'file2.json', 'json', 'json-result.json'],
-  ['file1.yaml', 'file2.yaml', 'json', 'json-result.json'],
-  ['file1.ini', 'file2.ini', 'json', 'json-result.json'],
-])('gendiff(%s, %s) to %s', (fileBefore, fileAfter, format, expected) => {
-  const result = genDiff(getFixturePath(fileBefore), getFixturePath(fileAfter), format);
-  expect(result).toBe(getResult(expected));
+const givenTwoFilesFileFormatAndOutputFormatterTypeAsParamsWhenGendiffIsCalledThenItShouldReturnExpectedResult = (testingFileFormat, outputFormatterType) => {
+  const pathFileBefore = getFixturePath(`file1.${testingFileFormat}`);
+  const pathFileAfter = getFixturePath(`file2.${testingFileFormat}`);
+  const expectedResult = getResult(`${outputFormatterType}-result.txt`);
+
+  const gendiffResult = genDiff(pathFileBefore, pathFileAfter, outputFormatterType);
+
+  expect(gendiffResult).toBe(expectedResult);
+};
+
+const twoTestingFilesFileFormats = ['json', 'yaml', 'ini'];
+
+const outputFormatterTypes = ['stylish', 'plain', 'json'];
+
+test.each(twoTestingFilesFileFormats)('gendiff with  two *.%s files', (testingFileFormat) => {
+  outputFormatterTypes.forEach((outputFormatterType) => {
+    givenTwoFilesFileFormatAndOutputFormatterTypeAsParamsWhenGendiffIsCalledThenItShouldReturnExpectedResult(testingFileFormat, outputFormatterType);
+  });
 });
